7205ab601742cc1210776f04e4e782a6173fef6a
sowjanya <sowju19@gmail.com>
2014-03-02 10:37:04 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contacttracker::Application.config.secret_key_base = '7dd93f837b73bfb3d712130d6649bca4be55a8ad49820093d91ade805cb34aaf0abd21bcc2d4b60b95affdd321c33c67657c9e536ff8077b9deffcd15e80d2da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Contacttracker::Application.config.session_store :cookie_store, key: '_contacttracker_session'
